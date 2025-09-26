b9f35d2ab68defcd7bf1a5f2aa9da1ad919dd39d
William Yarian <williamalexanderyarian@gmail.com>
2025-09-26 12:58:36 -0600

    Update homepage.ts

--- tests/pageObjects/homepage.ts
+++ tests/pageObjects/homepage.ts
@@ -1,1 +1,29 @@
+import { Page } from "@playwright/test";
+import { BasePage } from "./basePage";
 
+export class HomePage extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  // va a usar los mismos selectores que tus tests actuales
+  async search(term: string) {
+    const search = this.page.getByPlaceholder("Search");
+    await search.fill(term);
+    await search.press("Enter");
+  }
+
+  async clickProductByText(name: string) {
+    await this.page.getByText(name).click();
+  }
+
+  async openHome() {
+    await this.goto("https://opencart.abstracta.us/");
+  }
+
+  async takeTimestampedScreenshot(dir = "./captures") {
+    const filename = `${dir}/${Date.now()}-screenshot.jpg`;
+    await this.screenshot(filename);
+    return filename;
+  }
+}
